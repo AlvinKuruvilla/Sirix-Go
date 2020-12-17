9ed90842587537523c13bfd8c98ebd36ebbe42bc
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2020-12-16 20:50:12 -0500

    Refactored auth

--- internal/auth.go
+++ internal/auth.go
@@ -2,14 +2,14 @@ package internal
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/emirpasic/gods/maps/hashmap"
 )
 
 type scheme int
-
-var headerMap = hashmap.New()
 
 const (
 	//HTTP usng the HTTP protocol
@@ -25,45 +25,60 @@ var schemeStr = [...]string{
 
 //Auth a struct representing the data needed for authentication
 type Auth struct {
-	username  string
-	password  string
-	baseURL   string
-	client    http.Client
-	tokenData TokeData
-	s         scheme
-	host      string
+	username string
+	password string
+	client   http.Client
+	url      string
 }
 
 //NewAuth creates a new Auth struct
 func NewAuth(
 	username string,
 	password string,
-	baseURL string,
 	client http.Client,
-	s scheme,
-	host string) Auth {
-	emptyTokenData := TokeData{"", 0, 0, 0, 0, "", "", "", ""}
+	url string,
+) Auth {
 	a := Auth{
 		username,
 		password,
-		baseURL,
 		client,
-		emptyTokenData,
-		s,
-		host,
+		url,
 	}
 	return a
 }
 
-//NOTE: right now this is synchronous
-func (a Auth) Authenticate(w http.ResponseWriter, r *http.Request, jsonByteData []byte) {
+func (a Auth) Authenticate(jsonByteData []byte) *http.Response {
 	reqBody := bytes.NewBuffer(jsonByteData)
-	req, err := http.NewRequest("POST", a.baseURL, reqBody)
+	req, err := http.NewRequest("POST", a.url, reqBody)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
+	return resp
+}
+
+//Parse Parses the token data and refreshes access token before expirary
+func Parse(res http.Response) {
+	//data = {k.replace("-", "_"): v for k, v in resp.json().items()} - what is this doing and how do i replicate it here ??
+
+	// IDK what to do with this
+	//	self._token_data = TokenData(**data)
+	//	self._client.headers[
+	//		"Authorization"
+	//	] = f"{self._token_data.token_type} {self._token_data.access_token}"
+	//	self._timer = Timer(self._token_data.expires_in - 10, self._refresh)
+	//  self._timer = ensure_future(self._sleep_then_refresh())
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(string(body))
+	var parsed []string
+	if err := json.Unmarshal(body, &parsed); err != nil {
+		log.Fatalf("JSON unmarshal: %s", err)
+	}
 
 }
